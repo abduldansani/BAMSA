37692e7a00feb652a0294bd0d74df20a683cf467
abduldansani <abduldansani@gmail.com>
2024-01-10 00:30:32 +0100

    BAMSA OFFICIAL WEBSITE

---
+++ build/script.js
@@ -0,0 +1,26 @@
+const menuBtn = document.getElementById('menu_btn');
+const nav = document.getElementById('menu');
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('open');
+    nav.classList.toggle('flex');
+    nav.classList.toggle('hidden');
+}
+
+const customGreetings = [
+    { greeting: "Good morning!", startTime: 5, endTime: 11 },
+    { greeting: "Good afternoon!", startTime: 12, endTime: 16 },
+    { greeting: "Good evening!", startTime: 17, endTime: 23 }
+];
+
+function getGreeting() {
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+    const selectedGreeting = customGreetings.find(g => currentHour >= g.startTime && currentHour <= g.endTime);
+    if (!selectedGreeting && currentHour >= 0 && currentHour < 5) {
+        return "Up late, huh?";
+    }
+    return selectedGreeting ? selectedGreeting.greeting : "Hello!";
+}
+
+document.getElementById('greeting').innerHTML = getGreeting();
